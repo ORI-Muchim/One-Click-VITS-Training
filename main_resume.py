c235911a46b654f5d726fa4b2952f35da3a6988d
ORI-Muchim <whals467746@gmail.com>
2023-06-20 23:26:13 +0900

    Edit main_resume.py

--- main_resume.py
+++ main_resume.py
@@ -2,10 +2,10 @@ import os
 import subprocess
 import sys
 
-config_path = f"./datasets/{sys.argv[1]}.json"
+config_path = f"../datasets/{sys.argv[1]}.json"
 model_name = sys.argv[1]
 
-os.chdir('../vits')
+os.chdir('./vits')
 command = ["python", "train_ms.py", "-c", config_path, "-m", model_name]
 
 subprocess.run(command)
