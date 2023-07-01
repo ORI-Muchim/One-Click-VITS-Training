a0280946c03854fc7eeb63e0ef814fb20db9e1a6
ORI-Muchim <whals467746@gmail.com>
2023-07-01 18:25:55 +0900

    Edit integral.py

--- datasets/integral.py
+++ datasets/integral.py
@@ -95,9 +95,9 @@ def first_code():
         # Change the arguments and the input file here
         input_file = file_location
         output_dir = output_dir
-        min_silence_length = 0.6  # The minimum length of silence at which a split may occur [seconds]. Defaults to 3 seconds.
-        silence_threshold = 1e-3  # The energy level (between 0.0 and 1.0) below which the signal is regarded as silent.
-        step_duration = min_silence_length/10   # The amount of time to step forward in the input file after calculating energy. Smaller value = slower, but more accurate silence detection. Larger value = faster, but might miss some split opportunities. Defaults to (min-silence-length / 10.).
+        min_silence_length = 0.6
+        silence_threshold = 1e-3
+        step_duration = min_silence_length/10
 
 
         input_filename = input_file
